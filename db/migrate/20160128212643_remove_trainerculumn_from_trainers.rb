318bac35de423c4cdb0bd386cb7547cf7db45803
tripfrog <abe@tripfrog-design.com>
2016-01-30 22:25:48 +0900

    無駄なマイグレーションファイルの削除

--- db/migrate/20160128212643_remove_trainerculumn_from_trainers.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTrainerculumnFromTrainers < ActiveRecord::Migration
-  def change
-    remove_column :trainers, :category, :string
-    remove_column :trainers, :place, :string
-  end
-end
